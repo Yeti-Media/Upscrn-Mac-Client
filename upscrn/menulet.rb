59b6e5abca834aa42157fef6930c02168422958e
Matt Cowley <madcowley@gmail.com>
2011-09-14 10:45:46 -0400

    menulet

--- upscrn/menulet.rb
+++ upscrn/menulet.rb
@@ -5,5 +5,16 @@
 #  Created by Matthew Cowley on 9/5/11.
 #  Copyright 2011 __MyCompanyName__. All rights reserved.
 #
-
+class Menulet < NSObject
+    #def awakeFromNib
+    
+    #   statusItem = NSStatusBar.systemStatusBar.statusItemWithLength(NSVariableStatusItemLength)
+    #    statusItem.setHighlightMode(YES)
+    #    statusItem.setTitle("upscrn") 
+    #    statusItem.setEnabled(YES)
+    #    statusItem.setToolTip("upscrn")
+        
+    #    statusItem.setTarget(self)
+    #end
+end
 
